36a9b9ca1a996984a77deeb4a86fbb913ba610cf
PennyAna <law07006@byui.edu>
2020-10-31 22:58:47 -0600

    halloween eve work building project 1

--- web/dbConnect.php
+++ web/dbConnect.php
@@ -1,21 +1,23 @@
-<?php
 function get_db() {
-    $db = NULL;
-    try {
-        $dbUrl = getenv('DATABASE_URL');
-        $dbOpts = parse_url($dbUrl);
-        $dbHost = $dbOpts["host"];
-        $dbPort = $dbOpts["port"];
-        $dbUser = $dbOpts["user"];
-        $dbPassword = $dbOpts["pass"];
-        $dbName = ltrim($dbOpts["path"],'/');
-        $db = new PDO("psql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
-        $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    }
-    catch (PDOException $ex) {
-        echo 'Error!: ' . $ex->getMessage();
-        die();
-    }
-    return $db;
+	$db = NULL;
+
+	try {
+		$dbUrl = getenv('DATABASE_URL');
+		$dbopts = parse_url($dbUrl);
+		$dbHost = $dbopts["host"];
+		$dbPort = $dbopts["port"];
+		$dbUser = $dbopts["user"];
+		$dbPassword = $dbopts["pass"];
+		$dbName = ltrim($dbopts["path"],'/');
+
+		$db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
+
+		$db->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
+	}
+	catch (PDOException $ex) {
+		echo "Error connecting to DB. Details: $ex";
+		die();
+	}
+
+	return $db;
 }
-?>
