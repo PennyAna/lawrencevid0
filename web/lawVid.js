03ccfc897e34de15473baa407173077c3a6d6449
PennyAna <law07006@byui.edu>
2020-11-03 13:05:17 -0700

    js fix

--- web/lawVid.js
+++ web/lawVid.js
@@ -1,11 +1,12 @@
-const formSearch = document.getElementById("searchForm");
+
 //const formSearchData = new FormData(formSearch);
-const formNew = document.getElementById("newMovieForm");
+
 //const formNewData = new FormData(formNew);
-const formEdit = document.getElementById("editMovieForm");
+
 //const formEditData = new FormData(formEdit);
 
 function checkNewForm() {
+    const formNew = document.getElementById("newMovieForm");
     const newTitle = formNew.getElementById("titleName0");
     const newInfo = formNew.getElementById("titleInfo0");
     const titleError = formNew.getElementById("titleNameError0");
@@ -14,17 +15,19 @@ function checkNewForm() {
     checkMovieInfo(newInfo, infoError);
 }
 function checkEditForm() {
-    const editTitle = formNew.getElementById("titleName1");
-    const editInfo = formNew.getElementById("titleInfo1");
-    const titleError = formNew.getElementById("titleNameError1");
-    const infoError = formNew.getElementById("titleInfoError1");
+    const formEdit = document.getElementById("editMovieForm");
+    const editTitle = formEdit.getElementById("titleName1");
+    const editInfo = formEdit.getElementById("titleInfo1");
+    const titleError = formEdit.getElementById("titleNameError1");
+    const infoError = formEdit.getElementById("titleInfoError1");
     checkMovieName(editTitle, titleError);
     checkMovieInfo(editInfo, infoError);
 }
 
 function checkTitleSearch(title) {
-    const searchError = searchTitleForm.getElementByClass("errorMsg");
-    const searchField = searchTitleForm.getElementById("searchMovieName");
+    const formSearch = document.getElementById("searchForm");
+    const searchError = formSearch.getElementByClass("errorMsg");
+    const searchField = formSearch.getElementById("searchMovieName");
     if (searchField.value == "") {
         searchError.display = compact;
         searchError.innerHTML = "Did you forget to put in the name?";
