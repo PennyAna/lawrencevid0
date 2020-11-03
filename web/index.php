de02505aee1da366272ce27b9d968abd1842458f
PennyAna <law07006@byui.edu>
2020-11-03 15:39:24 -0700

    js fix

--- web/index.php
+++ web/index.php
@@ -1,4 +1,5 @@
 <?php
+session_start();
 require("dbConnect.php");
 $db = get_db();
 ?>
--- web/lawVid.js
+++ web/lawVid.js
@@ -1,14 +1,20 @@
+document.getElementById("searchTitleError").addEventListener("onblur", checkTitleSearch());
+document.getElementById("titleName0").addEventListener("onblur", checkNewForm());
+document.getElementById("titleInfo0").addEventListener("onblur", checkNewForm());
+document.getElementById("titleName1").addEventListener("onblur", checkEditForm());
+document.getElementById("titleInfo1").addEventListener("onblur", checkEditForm());
+
 function checkNewForm() {
-    const newTitle = document.getElementById("titleName0");
-    const newInfo = document.getElementById("titleInfo0");
+    const newTitle = document.getElementById("titleName0").value;
+    const newInfo = document.getElementById("titleInfo0").value;
     const titleError = document.getElementById("titleNameError0");
     const infoError = document.getElementById("titleInfoError0");
     checkMovieName(newTitle, titleError);
     checkMovieInfo(newInfo, infoError);
 }
 function checkEditForm() {
-    const editTitle = document.getElementById("titleName1");
-    const editInfo = document.getElementById("titleInfo1");
+    const editTitle = document.getElementById("titleName1").value;
+    const editInfo = document.getElementById("titleInfo1").value;
     const titleError = document.getElementById("titleNameError1");
     const infoError = document.getElementById("titleInfoError1");
     checkMovieName(editTitle, titleError);
