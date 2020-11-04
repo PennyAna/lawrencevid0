5d9a178547e2bee77fe43b932011b513be28f651
PennyAna <law07006@byui.edu>
2020-11-03 18:39:01 -0700

    css fix

--- web/index.php
+++ web/index.php
@@ -17,7 +17,7 @@ $db = get_db();
         <nav class="navbar navbar-fixed-top">
             <div class="container-fluid">
             <div class="navbar-header">
-                <a class="navbar-brand" href="#">Lawrence Family Video</a>
+                <a class="navbar-brand nav-justified" href="#">Lawrence Family Video</a>
             </div>
             <button class="btn navbar-btn" onclick="location.href='insert.php'">Add New Movie</button>
             <button class="btn navbar-btn" onclick="location.href='alpha.php'">Search Alphabetically</button>
